3123e76cb23a948179b1fb25a6e8511bcc95a075
BlackWorld72 <twix72000@gmail.com>
2022-12-17 16:27:14 +0100

    split anglais

---
+++ js/anglaislecon.js
@@ -0,0 +1,451 @@
+anglais =
+	{
+		"cat": [
+			{
+				"nom": " L1 - What et which",
+				"words": [
+					{
+						"fr": "What ",
+						"al": " Annonce une information (début de phrase mais pas que)"
+					},
+					{
+						"fr": "Which ",
+						"al": " Reprend ce qui a été dit avant (milieu de phrase)"
+					},
+					{
+						"fr": "Traduction what et which ",
+						"al": " ce qui, ce que, ce dont"
+					}
+				]
+			},
+			{
+				"nom": " L2 - A et an",
+				"words": [
+					{
+						"fr": "A ",
+						"al": " Devant une consonne + ce qui se prononce \"you\" (university)"
+					},
+					{
+						"fr": "An ",
+						"al": " Devant une voyelle"
+					}
+				]
+			},
+			{
+				"nom": " L3 - Since et for",
+				"words": [
+					{
+						"fr": "Since ",
+						"al": " Date / Moments précis du passé (toujours au passé)"
+					},
+					{
+						"fr": "For ",
+						"al": " Durée (peut être au passé et au futur)"
+					}
+				]
+			},
+			{
+				"nom": " L4 - Prononciation ed ",
+				"words": [
+					{
+						"fr": "Ce qui finit par k, s, ch, sh, the, p, gh ",
+						"al": " se prononce \"t\""
+					},
+					{
+						"fr": "Ce qui finit par t, d ",
+						"al": " se prononce \"id\""
+					},
+					{
+						"fr": "Ce qui finit par le reste ",
+						"al": " se prononce \"d\""
+					}
+				]
+			},
+			{
+				"nom": " L5 - Dérivation",
+				"words": [
+					{
+						"fr": "Forme en ed ",
+						"al": " Correspond à -é en français"
+					},
+					{
+						"fr": "Forme en ing ",
+						"al": " Correspond à -ant en français "
+					},
+					{
+						"fr": "Nom -> Adjectif ",
+						"al": " +able / +al / +ate / +en / +etic / +ful / +ible / +ic / +ical / +ish / +less / +ly / +ous / +y"
+					},
+					{
+						"fr": "Verbe -> Nom ",
+						"al": " +al / + ant / +ance / +ation / + ative / + er / +ion / +ment / + or / +our // - ed"
+					}
+				]
+			},
+			{
+				"nom": " L6 - Look, watch, see",
+				"words": [
+					{
+						"fr": "See ",
+						"al": " Capacité de voir "
+					},
+					{
+						"fr": "Look ",
+						"al": " Regarder (qqlch d’immobile)"
+					},
+					{
+						"fr": "Watch ",
+						"al": " Regarder (qqlch d’animé)"
+					}
+				]
+			},
+			{
+				"nom": " L7 - Comparison et Description ",
+				"words": [
+					{
+						"fr": "Comparaison ",
+						"al": " Sens + like + nom"
+					},
+					{
+						"fr": "Description ",
+						"al": " Sens + as if / though + sentence"
+					}
+				]
+			},
+			{
+				"nom": " L8 - Adjectives ed-ing",
+				"words": [
+					{
+						"fr": "Ing ",
+						"al": " Description d’un effet"
+					},
+					{
+						"fr": "Ed ",
+						"al": " Description d’un état"
+					}
+				]
+			},
+			{
+				"nom": " L9 - Locations ",
+				"words": [
+					{
+						"fr": "Towards ",
+						"al": " En direction de "
+					},
+					{
+						"fr": "To ",
+						"al": " Mouvement"
+					},
+					{
+						"fr": "Eastern + nom ",
+						"al": " Adj est"
+					},
+					{
+						"fr": "Western + nom ",
+						"al": " Adj ouest"
+					},
+					{
+						"fr": "Southern + nom ",
+						"al": " Adj sud"
+					},
+					{
+						"fr": "Northern + nom ",
+						"al": " Adj nord "
+					}
+				]
+			},
+
+			{
+				"nom": " L10 - Probability",
+				"words": [
+					{
+						"fr": "Mod + BV ",
+						"al": " Généralité"
+					},
+					{
+						"fr": "Mod + Being ",
+						"al": " En train de "
+					},
+					{
+						"fr": "Mod + Have + Part passé ",
+						"al": " Probabilité passé"
+					},
+					{
+						"fr": "99% modaux ",
+						"al": " Must"
+					},
+					{
+						"fr": "50% modaux ",
+						"al": " Can/Could/May"
+					},
+					{
+						"fr": "10% modaux ",
+						"al": " Might"
+					},
+					{
+						"fr": "99% adverbes ",
+						"al": " Probably/Certainly/Surely"
+					},
+					{
+						"fr": "50/10% adverbes ",
+						"al": " Possibly/Maybe/Perhaps"
+					}
+				]
+			},
+
+			{
+				"nom": " L11 - Habits",
+				"words": [
+					{
+						"fr": "Passé révolu",
+						"al": "X used to + BV (pas ing)"
+					},
+					{
+						"fr": "Derrière mots ",
+						"al": " Verbe en -Ing ou un nom"
+					},
+					{
+						"fr": "Habitude déjà acquise ",
+						"al": " To be used to / To be accustomed to"
+					},
+					{
+						"fr": "Habitude que l’on prend ",
+						"al": " To get used to / To get accustomed to"
+					}
+				]
+			},
+
+			{
+				"nom": " L12 - Dénombrable/Indénombrables",
+				"words": [
+					{
+						"fr": "Noms dénombrables règles ",
+						"al": " Pluriel possible "
+					},
+					{
+						"fr": "Noms indénombrables règles ",
+						"al": " Pluriel impossible "
+					},
+					{
+						"fr": "Noms indénombrables catégories ",
+						"al": " Matière/liquide/notions abstraites (time, love..)"
+					},
+					{
+						"fr": "Noms dénombrables mots ",
+						"al": " Many/lot of"
+					},
+					{
+						"fr": "Noms indénombrables mots ",
+						"al": " Much/A lot of"
+					}
+				]
+			},
+
+			{
+				"nom": " L13 - Notion/Abstrait",
+				"words": [
+					{
+						"fr": "+hood ",
+						"al": " notion/abstrait"
+					},
+					{
+						"fr": "exemples ",
+						"al": " childhood/adulthood"
+					}
+				]
+			},
+
+			{
+				"nom": " L14 - Pronoms",
+				"words": [
+					{
+						"fr": "Pronoms personnels sujets ",
+						"al": " I, You, He/She/It, We, You, They"
+					},
+					{
+						"fr": "Pronoms personnels compléments ",
+						"al": " Me, You, Him/Her/It, Us, You, Them"
+					},
+					{
+						"fr": "Pronom générique ",
+						"al": " One (on)"
+					},
+					{
+						"fr": "Adjectifs possessifs ",
+						"al": " One’s (on), My, Your, His/Her/Its, Our, Your, Their"
+					},
+					{
+						"fr": "Pronom réfléchis ",
+						"al": " Oneself, Myself, Yourself, Himself/Herself/Itself, Ourselves, Yourselves, Themselves"
+					},
+					{
+						"fr": "Pronoms possessifs ",
+						"al": " Mine, Yours, His/Hers/Its, Ours, Yours, Theirs (le mien, le tiens..)"
+					}
+				]
+			},
+			{
+				"nom": " L15 - Quantifieurs",
+				"words": [
+					{
+						"fr": "Basic positif ",
+						"al": " Some"
+					},
+					{
+						"fr": "Humain positif ",
+						"al": " Someone / Somebody"
+					},
+					{
+						"fr": "Lieu positif ",
+						"al": " Somewhere"
+					},
+					{
+						"fr": "Chose ",
+						"al": " Something"
+					},
+					{
+						"fr": "Basic question ",
+						"al": " Any"
+					},
+					{
+						"fr": "Humain question ",
+						"al": " Anybody / Anyone "
+					},
+					{
+						"fr": "Lieu question ",
+						"al": " Anywhere"
+					},
+					{
+						"fr": "Chose question ",
+						"al": " Anything"
+					},
+					{
+						"fr": "Basique négatif ",
+						"al": " No / Not … any"
+					},
+					{
+						"fr": "Humain négatif ",
+						"al": " Nobody / Noone / Not…anybody / Not…anyone "
+					},
+					{
+						"fr": "Lieu négatif ",
+						"al": " Nowhere / Not…anywhere"
+					},
+					{
+						"fr": "Chose négatif ",
+						"al": " Nothing / Not…Anything"
+					}
+				]
+			},
+
+			{
+				"nom": " L16 - Le but",
+				"words": [
+					{
+						"fr": "To + BV ",
+						"al": " Pour"
+					},
+					{
+						"fr": "In order to + BV / So as to + BV ",
+						"al": " Afin de"
+					},
+					{
+						"fr": "So that… (S+Mod+BV) ",
+						"al": " Afin que"
+					},
+					{
+						"fr": "Exemple afin que ",
+						"al": " So that you can become.."
+					}
+				]
+			},
+
+			{
+				"nom": " L17 - Diplômes",
+				"words": [
+					{
+						"fr": "Baccalauréat professionnel ",
+						"al": " A vocational degree / A vocational Baccalaureate"
+					},
+					{
+						"fr": "O_levels .. (3subjects) ",
+						"al": " Première"
+					},
+					{
+						"fr": "A_levels (A=Advanced) ..(3subjects) ",
+						"al": " Terminal"
+					},
+					{
+						"fr": "Baccalauréat général / technologique in Great Britain ",
+						"al": " A levels in… (3 subjects)"
+					},
+					{
+						"fr": "Baccalauréat général / technologique in USA ",
+						"al": " High school graduation"
+					},
+					{
+						"fr": "Baccalauréat général / technologique S ",
+						"al": " A_levels in sciences, maths, chemistry, biology"
+					},
+					{
+						"fr": "Baccalauréat général / technologique L ",
+						"al": " A_levels in arts, literature, language, history"
+					},
+					{
+						"fr": "Baccalauréat général / technologique ES ",
+						"al": " A_levels in social science, economics"
+					},
+					{
+						"fr": "Baccalauréat général / technologique STD2A ",
+						"al": " A_levels in design and applied arts"
+					},
+					{
+						"fr": "BTS / IUT (1) ",
+						"al": " A 2-year technical degree in model making, patterning, fashion industry"
+					},
+					{
+						"fr": "BTS / IUT (2) ",
+						"al": " A H(igher) N(ational) D(egree) in…"
+					},
+					{
+						"fr": "BTS / IUT (3) ",
+						"al": " A 2-year post A_level technical degree in…"
+					},
+					{
+						"fr": "Licence (lettre / science) ",
+						"al": " A bachelor of… (arts / sciences)"
+					},
+					{
+						"fr": "Master (lettre / science) ",
+						"al": " A master of… (arts / sciences) "
+					}
+				]
+			},
+			{
+				"nom": " L18 - EE ER",
+				"words": [
+					{
+						"fr": "ER ",
+						"al": " Celui qui fait (Employer)"
+					},
+					{
+						"fr": "EE ",
+						"al": " Celui qui subit (Employee) "
+					}
+				]
+			},
+			{
+				"nom": " L19 - Substentivation",
+				"words": [
+					{
+						"fr": "A course that lasts two years ",
+						"al": " A two year course"
+					},
+					{
+						"fr": "Sujet + Verbe + Complement (Phrase normal) ",
+						"al": " A + Adj qualificatif (Complément) + Nom (Sujet) "
+					}
+				]
+			}
+		]
+	}
