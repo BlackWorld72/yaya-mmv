aae05d5156fbe9893065163ef764f975d0eb1178
BlackWorld72 <twix72000@gmail.com>
2022-12-21 22:46:55 +0100

    couture theorique

--- js/couture_tech.js
+++ js/couture_tech.js
@@ -288,6 +288,626 @@ couture_tech =
 					"al": " Assisté par ordinateur, traçage des axes coordonnées de chaque point qui se trouve sur le patron et placer les progressions "
 				}
 			]
-		}
+		},
+		{
+			"nom": " Processus de fabrication",
+			"words": [
+				{
+					"fr": "1 ",
+					"al": " Etudes des marchés (service mercatique)"
+				},
+				{
+					"fr": "2 ",
+					"al": " Cahier des charges (bureau d'études)"
+				},
+				{
+					"fr": "3 ",
+					"al": " Avant projet "
+				},
+				{
+					"fr": "4 ",
+					"al": " Prototypes (bureau d'études)"
+				},
+				{
+					"fr": "5 ",
+					"al": " Essais (direction, styliste, modéliste, service commerciale)"
+				},
+				{
+					"fr": "6 ",
+					"al": " Modèle retenu"
+				},
+				{
+					"fr": "7 ",
+					"al": " Projet définitif (bureau d'étude)"
+				},
+				{
+					"fr": "8 ",
+					"al": " Etudes de fabrication (bureau des méthodes)"
+				},
+				{
+					"fr": "9 ",
+					"al": " Lancement en série (planning) (bureau des méthodes)"
+				},
+				{
+					"fr": "10 ",
+					"al": " Coupe en série (services techniques)"
+				},
+				{
+					"fr": "11 ",
+					"al": " Fabrication en série (services techniques)"
+				},
+				{
+					"fr": "12 ",
+					"al": " Contrôles des articles (services techniques)"
+				},
+				{
+					"fr": "13 ",
+					"al": " Modèles terminé (stockage, expédition) (technicien, magsinier, manutentionnaire)"
+				},
+				{
+					"fr": "14 ",
+					"al": " Mise sur le marché, suivi du produit en clientèle (service vente et après vente)"
+				}
+			]
+		},
+
+		{
+			"nom": " Descriptif d'un vêtement",
+			"words": [
+				{
+					"fr": "A ",
+					"al": " Phrase d'introduction"
+				},
+				{
+					"fr": "B ",
+					"al": " Forme générale du vêtement"
+				},
+				{
+					"fr": "C ",
+					"al": " Les lignes visibles, les découpes"
+				},
+				{
+					"fr": "D ",
+					"al": " Les détails du haut vers le bas (cols, poches, etc)"
+				},
+				{
+					"fr": "E ",
+					"al": " Moyen de fermeture"
+				},
+				{
+					"fr": "/!\\ 1 ",
+					"al": " On peut ajouter style, saison, etc "
+				},
+				{
+					"fr": "/!\\ 2 ",
+					"al": " Employer termes exactes"
+				},
+				{
+					"fr": "/!\\ 3 ",
+					"al": " Eviter répétitions de vocabulaires"
+				},
+				{
+					"fr": "/!\\ 14 ",
+					"al": " Ne pas débuter par \"il y a\""
+				}
+			]
+		},
+		{
+			"nom": "Cols 1",
+			"words": [
+				{
+					"fr": "Encolure ras du cou",
+					"al": "<img> encolurerasducou"
+				},
+				{
+					"fr": "Encolure dégagée",
+					"al": "<img> encoluredegagee"
+				},
+				{
+					"fr": "Epaules tombantes",
+					"al": "<img> epaulestombantes"
+				},
+				{
+					"fr": "Trou de serrure",
+					"al": "<img> troudeserrure"
+				},
+				{
+					"fr": "Encolure en forme de \"U\"",
+					"al": "<img> encolureenformedeu"
+				},
+				{
+					"fr": "Encolure bateau",
+					"al": "<img> encolurebateau"
+				},
+				{
+					"fr": "Encolure bretelles",
+					"al": "<img> encolurebretelles"
+				},
+				{
+					"fr": "Encolure carrée",
+					"al": "<img> encolurecarree"
+				},
+				{
+					"fr": "Encolure en coeur",
+					"al": "<img> encolureencoeur"
+				},
+				{
+					"fr": "Encolure en \"V\"",
+					"al": "<img> encolureenv"
+				},
+				{
+					"fr": "Décolleté plongeant",
+					"al": "<img> decolleteplongeant"
+				},
+				{
+					"fr": "Col montant",
+					"al": "<img> colmontant"
+				},
+			]
+		},
+		{
+			"nom": "Cols 2",
+			"words": [
+				{
+					"fr": "Encolure drapée",
+					"al": "<img> encoluredrapee"
+				},
+				{
+					"fr": "Col officier",
+					"al": "<img> colofficier"
+				},
+				{
+					"fr": "Col chinois",
+					"al": "<img> colchinois"
+				},
+				{
+					"fr": "Encolure montante",
+					"al": "<img> encoluremontante"
+				},
+				{
+					"fr": "Col cassé",
+					"al": "<img> colcasse"
+				},
+				{
+					"fr": "Cache-coeur",
+					"al": "<img> cachecoeur"
+				},
+				{
+					"fr": "Encolure trapèze",
+					"al": "<img> encoluretrapeze"
+				},
+				{
+					"fr": "Trapèze inversé",
+					"al": "<img> trapezeinverse"
+				},
+				{
+					"fr": "Col châle",
+					"al": "<img> colchale"
+				},
+				{
+					"fr": "Col marin devant",
+					"al": "<img> colmarindevant"
+				},
+				{
+					"fr": "Col marin dos",
+					"al": "<img> colmarindos"
+				},
+				{
+					"fr": "Encolure asymétrique",
+					"al": "<img> encolureasymetrique"
+				},
+			]
+		},
+		{
+			"nom": "Cols 3",
+			"words": [
+				{
+					"fr": "Col Claudine",
+					"al": "<img> colclaudine"
+				},
+				{
+					"fr": "Col Claudine amovible",
+					"al": "<img> colclaudineamovible"
+				},
+				{
+					"fr": "Col chlorale",
+					"al": "<img> colchlorale"
+				},
+				{
+					"fr": "Collerette",
+					"al": "<img> collerette"
+				},
+				{
+					"fr": "Col Berthe",
+					"al": "<img> colberthe"
+				},
+				{
+					"fr": "Col cape",
+					"al": "<img> colcape"
+				},
+				{
+					"fr": "Col carré",
+					"al": "<img> colcarre"
+				},
+				{
+					"fr": "Col cygne",
+					"al": "<img> colcygne"
+				},
+				{
+					"fr": "Col à revers",
+					"al": "<img> colarevers"
+				},
+				{
+					"fr": "Col poête",
+					"al": "<img> colpoete"
+				},
+				{
+					"fr": "Col banane",
+					"al": "<img> colbanane"
+				},
+			]
+		},
+		{
+			"nom": "Cols 4",
+			"words": [
+				{
+					"fr": "Col Napoléon",
+					"al": "<img> colnapoleon"
+				},
+				{
+					"fr": "Col élisabéthain",
+					"al": "<img> colelisabethain"
+				},
+				{
+					"fr": "Col chemisier",
+					"al": "<img> colchemisier"
+				},
+				{
+					"fr": "Col roulé",
+					"al": "<img> colroule"
+				},
+				{
+					"fr": "Col chemisier boutonné",
+					"al": "<img> colchemisierboutonne"
+				},
+				{
+					"fr": "Col chemisier à pointes allongées",
+					"al": "<img> colchemisierapointesallongees"
+				},
+				{
+					"fr": "Col boucle",
+					"al": "<img> colboucle"
+				},
+				{
+					"fr": "Col noué",
+					"al": "<img> colnoue"
+				},
+				{
+					"fr": "Col ascot",
+					"al": "<img> colascot"
+				},
+			]
+		},
+		{
+			"nom": "Manches 1",
+			"words": [
+				{
+					"fr": "Manche montée",
+					"al": "<img> manchemontee"
+				},
+				{
+					"fr": "Manche tailleur",
+					"al": "<img> manchetailleur"
+				},
+				{
+					"fr": "Manche ajustée ou coudée",
+					"al": "<img> mancheajusteeoucoudee"
+				},
+				{
+					"fr": "Manche chemisier",
+					"al": "<img> manchechemisier"
+				},
+				{
+					"fr": "Manche courte",
+					"al": "<img> manchecourte"
+				},
+				{
+					"fr": "Manche raglan",
+					"al": "<img> mancheraglan"
+				},
+			]
+		},
+		{
+			"nom": "Manches 2",
+			"words": [
+				{
+					"fr": "Manche dolman",
+					"al": "<img> manchedolman"
+				},
+				{
+					"fr": "Manche chauve-souris",
+					"al": "<img> manchechauvesouris"
+				},
+				{
+					"fr": "Manche cloche",
+					"al": "<img> manchecloche"
+				},
+				{
+					"fr": "Manche lanterne",
+					"al": "<img> manchelanterne"
+				},
+				{
+					"fr": "Manche bishop",
+					"al": "<img> manchebishop"
+				},
+				{
+					"fr": "Manche à plis",
+					"al": "<img> mancheaplis"
+				},
+			]
+		},
+		{
+			"nom": "Manches 3",
+			"words": [
+				{
+					"fr": "Manche à volant",
+					"al": "<img> mancheavolant"
+				},
+				{
+					"fr": "Manche ballon",
+					"al": "<img> mancheballon"
+				},
+				{
+					"fr": "Manche paysan",
+					"al": "<img> manchepaysan"
+				},
+				{
+					"fr": "Manche bouffante",
+					"al": "<img> manchebouffante"
+				},
+				{
+					"fr": "Manche bouillonnée",
+					"al": "<img> manchebouillonnee"
+				},
+				{
+					"fr": "Manche à godets",
+					"al": "<img> mancheagodets"
+				},
+				{
+					"fr": "Manche courte ouverte",
+					"al": "<img> manchecourteouverte"
+				},
+				{
+					"fr": "Manche calypso",
+					"al": "<img> manchecalypso"
+				},
+			]
+		},
+		{
+			"nom": "Manches 4",
+			"words": [
+				{
+					"fr": "Manche pagode",
+					"al": "<img> manchepagode"
+				},
+				{
+					"fr": "Manche gigot",
+					"al": "<img> manchegigot"
+				},
+				{
+					"fr": "Manche ange",
+					"al": "<img> mancheange"
+				},
+				{
+					"fr": "Manche bracelet",
+					"al": "<img> manchebracelet"
+				},
+				{
+					"fr": "Manche tulipe",
+					"al": "<img> manchetulipe"
+				},
+				{
+					"fr": "Manche caftan",
+					"al": "<img> manchecaftan"
+				},
+				{
+					"fr": "Mancheron",
+					"al": "<img> mancheron"
+				},
+				{
+					"fr": "Manche kimono traditionnelle",
+					"al": "<img> manchekimonotraditionnelle"
+				},
+			]
+		},
+		{
+			"nom": "Poignets",
+			"words": [
+				{
+					"fr": "Poignet mousquetaire",
+					"al": "<img> poignetmousquetaire"
+				},
+				{
+					"fr": "Poignet chemisier",
+					"al": "<img> poignetchemisier"
+				},
+				{
+					"fr": "Manchette",
+					"al": "<img> manchette"
+				},
+				{
+					"fr": "Poignet avec pinces plis",
+					"al": "<img> poignetavecpincesplis"
+				},
+				{
+					"fr": "Poignet roulé",
+					"al": "<img> poignetroule"
+				},
+				{
+					"fr": "Poignet à glissière",
+					"al": "<img> poignetaglissiere"
+				},
+				{
+					"fr": "Poignet bords côtes",
+					"al": "<img> poignetbordscotes"
+				},
+				{
+					"fr": "Poignet à coulisse",
+					"al": "<img> poignetacoulisse"
+				},
+			]
+		},
+		{
+			"nom": "Poches 1",
+			"words": [
+				{
+					"fr": "Poche plaquée",
+					"al": "<img> pocheplaquee"
+				},
+				{
+					"fr": "Poche plaquée surpiquée",
+					"al": "<img> pocheplaqueesurpiquee"
+				},
+				{
+					"fr": "Poche plaquée passepoilée",
+					"al": "<img> pocheplaqueepassepoilee"
+				},
+				{
+					"fr": "Poche plaquée surpiquée avec rabat",
+					"al": "<img> pocheplaqueesurpiqueeavecrabat"
+				},
+				{
+					"fr": "Poche plaquée à rabat, bords arrondis",
+					"al": "<img> pocheplaqueearabatbordsarrondis"
+				},
+				{
+					"fr": "Poche passepoilée surpiquée",
+					"al": "<img> pochepassepoileesurpiquee"
+				},
+				{
+					"fr": "Poche passepoilée avec renforts",
+					"al": "<img> pochepassepoileeavecrenforts"
+				},
+				{
+					"fr": "Poche passepoilée",
+					"al": "<img> pochepassepoilee"
+				},
+			]
+		},
+		{
+			"nom": "Poches 2",
+			"words": [
+				{
+					"fr": "Poche passepoilée à glissière",
+					"al": "<img> pochepassepoileeaglissiere"
+				},
+				{
+					"fr": "Poche passepoilée boutonnée avec patte",
+					"al": "<img> pochepassepoileeboutonneeavecpatte"
+				},
+				{
+					"fr": "Poche revers",
+					"al": "<img> pocherevers"
+				},
+				{
+					"fr": "Poche passepoilée à revers",
+					"al": "<img> pochepassepoileearevers"
+				},
+				{
+					"fr": "Poche ville",
+					"al": "<img> pocheville"
+				},
+				{
+					"fr": "Poche kangourou",
+					"al": "<img> pochekangourou"
+				},
+				{
+					"fr": "Poche couture",
+					"al": "<img> pochecouture"
+				},
+				{
+					"fr": "Poche italienne",
+					"al": "<img> pocheitalienne"
+				},
+			]
+		},
+		{
+			"nom": "Plis et ouvertures 1",
+			"words": [
+				{
+					"fr": "Smocks",
+					"al": "<img> smocks"
+				},
+				{
+					"fr": "Plissé bijou",
+					"al": "<img> plissebijou"
+				},
+				{
+					"fr": "Double soufflé renforcé par des coutures en triangle",
+					"al": "<img> doublesoufflerenforcepardescouturesentriangle"
+				},
+				{
+					"fr": "Patte de serrage",
+					"al": "<img> pattedeserrage"
+				},{
+					"fr": "Fronces",
+					"al": "<img> fronces"
+				},
+				{
+					"fr": "Pli creux",
+					"al": "<img> plicreux"
+				},
+				{
+					"fr": "Pli religieuse",
+					"al": "<img> plireligieuse"
+				},
+				{
+					"fr": "Plis plats pressés",
+					"al": "<img> plisplatspresses"
+				},
+
+			]
+		},
+		{
+			"nom": "Plis et ouvertures 2",
+			"words": [
+				{
+					"fr": "Pli rond",
+					"al": "<img> plirond"
+				},
+				{
+					"fr": "Plis plats",
+					"al": "<img> plisplats"
+				},
+				{
+					"fr": "Plissé soleil",
+					"al": "<img> plissesoleil"
+				},
+				{
+					"fr": "Pince-pli",
+					"al": "<img> pincepli"
+				},
+				{
+					"fr": "Plis souples retenus",
+					"al": "<img> plissouplesretenus"
+				},
+				{
+					"fr": "Fentes latérales",
+					"al": "<img> fenteslaterales"
+				},
+				{
+					"fr": "Pli d'aisance",
+					"al": "<img> plidaisance"
+				},
+				{
+					"fr": "Fente d'aisance",
+					"al": "<img> fentedaisance"
+				},
+				{
+					"fr": "Ouverture d'aisance",
+					"al": "<img> ouverturedaisance"
+				}
+			]
+		},
 	] 
 }
